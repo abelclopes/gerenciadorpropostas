462c166eec185d5191ae5c7bad2cdb60bd06cfe7
Abel Lopes <abellopes@gmail.com>
2018-05-10 13:19:08 -0300

    modulo categoria

--- cliente/src/app/app.module.ts
+++ cliente/src/app/app.module.ts
@@ -9,17 +9,16 @@ import { NgxDatatableModule } from '@swimlane/ngx-datatable';
 import {NgxPaginationModule} from 'ngx-pagination';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { AuthService } from './logica-api';
-import { LoginModule } from './login/login.module';
-import { AuthenticationService } from './login/service/authentication.service';
+import { AuthenticationService } from './components/login/service/authentication.service';
 import { AuthGuard } from './_guards/auth.guard';
-import { HeaderComponent } from './header/header.component';
+import { HeaderComponent } from './components/header/header.component';
 import { SharedModule } from './shared/shared.module';
 import { NotificationService } from './shared/messages/notification.service';
-import { HeaderService } from './header/header.service';
-import { CategoriaService } from './categorias/service/categoria.service';
+import { HeaderService } from './components/header/header.service';
 import { PaginationFilter } from './shared/pagination-filter.pipe';
+import { LoginModule } from './components/login/login.module';
 
 
 @NgModule({
@@ -44,7 +43,6 @@ import { PaginationFilter } from './shared/pagination-filter.pipe';
   ],
   providers: [
     AuthenticationService,
-    CategoriaService,
     AuthGuard,
     NotificationService, HeaderService,
     PaginationFilter
