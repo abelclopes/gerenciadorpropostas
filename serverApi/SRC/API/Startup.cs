5f9ae8c296425d63e3c91363236b0e2da41ced87
Abel Lopes <abellopes@gmail.com>
2018-05-23 16:35:37 -0300

    Microsoft.AspNetCore.Authorization

--- serverApi/SRC/API/Startup.cs
+++ serverApi/SRC/API/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.Authorization;
 //using Swashbuckle.AspNetCore.Swagger;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
